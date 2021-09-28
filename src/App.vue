7fc04c8a8b95b3be7663931945a72639dc027ddd
DarioDa86 <dario.dagnelli@gmail.com>
2021-09-28 20:19:08 +0200

    Esercizio completato

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Title/>
+    <img alt="Labirinto" src="./assets/Cubo-labirinto.jpg">
+  </div>
+</template>
+
+<script>
+import Title from './components/Title.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Title
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2e3133;
+  margin-top: 40px;
+}
+</style>
